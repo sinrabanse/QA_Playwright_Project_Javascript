a31beab90d358afc471b22ac9cf124048ad0ed68
Aleksandr Arkhipov <130854915+sinrabanse@users.noreply.github.com>
2024-11-21 17:55:59 +0200

    Parameterize tests

--- tests/loginTests/negativeTests/ValidateUsersLoginNegative.spec.js
+++ tests/loginTests/negativeTests/ValidateUsersLoginNegative.spec.js
@@ -4,82 +4,47 @@ import { testLocators } from "../../utils/testLocators.js";
 import { websiteURL } from "../../utils/testURL.js";
 import { testAssertions } from "../../utils/testAssertions.js";
 
-const users = [testData.standard_user, testData.incorrect_standard_user];
-
-const passwords = [testData.password, testData.incorrect_password];
-
-test.describe("IncorrectPassword", () => {
-  for (const user of users) {
-    test(`For ${user}`, async ({ page }) => {
+[
+  {
+    user: testData.standard_user,
+    password: testData.incorrect_password,
+    error_message: testAssertions.wrong_pass_or_user_text,
+  },
+  {
+    user: testData.incorrect_standard_user,
+    password: testData.incorrect_password,
+    error_message: testAssertions.wrong_pass_or_user_text,
+  },
+  {
+    user: testData.incorrect_standard_user,
+    password: testData.password,
+    error_message: testAssertions.wrong_pass_or_user_text,
+  },
+  {
+    user: testData.standard_user,
+    password: null,
+    error_message: testAssertions.requared_password_message,
+  },
+  {
+    user: null,
+    password: testData.password,
+    error_message: testAssertions.requared_username_message,
+  },
+  {
+    user: null,
+    password: null,
+    error_message: testAssertions.requared_username_message,
+  },
+].forEach(({ user, password, error_message }) => {
+  test.describe(`Testing with user: ${user}, password: ${password}`, () => {
+    test(`Valid`, async ({ page }) => {
       await page.goto(websiteURL.baseURL);
-      await page.locator(testLocators.username_field).fill(`${user}`);
-      await page
-        .locator(testLocators.password_field)
-        .fill(testData.incorrect_password);
+      await page.locator(testLocators.username_field).fill(user || "");
+      await page.locator(testLocators.password_field).fill(password || "");
       await page.locator(testLocators.login_button).click();
       await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-        testAssertions.wrong_pass_or_user_text
+        error_message
       );
     });
-  }
-});
-
-test.describe("IncorrectUser", () => {
-  for (const password of passwords) {
-    test(`For ${password}`, async ({ page }) => {
-      await page.goto(websiteURL.baseURL);
-      await page
-        .locator(testLocators.username_field)
-        .fill(testData.incorrect_standard_user);
-      await page.locator(testLocators.password_field).fill(`${password}`);
-      await page.locator(testLocators.login_button).click();
-      await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-        testAssertions.wrong_pass_or_user_text
-      );
-    });
-  }
-});
-
-test.describe("IncorrectUserAndPassword", () => {
-  test(`Valid`, async ({ page }) => {
-    await page.goto(websiteURL.baseURL);
-    await page
-      .locator(testLocators.username_field)
-      .fill(testData.incorrect_standard_user);
-    await page
-      .locator(testLocators.password_field)
-      .fill(testData.incorrect_password);
-    await page.locator(testLocators.login_button).click();
-    await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-      testAssertions.wrong_pass_or_user_text
-    );
-  });
-});
-
-test.describe("Empty Fields", () => {
-  test("Correct User Empty Password", async ({ page }) => {
-    await page.goto(websiteURL.baseURL);
-    await page
-      .locator(testLocators.username_field)
-      .fill(testData.standard_user);
-    await page.locator(testLocators.login_button).click();
-    await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-      testAssertions.requared_password_message
-    );
-  });
-  test("Empty User Correct Password", async ({ page }) => {
-    await page.goto(websiteURL.baseURL);
-    await page.locator(testLocators.password_field).fill(testData.password);
-    await page.locator(testLocators.login_button).click();
-    await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-      testAssertions.requared_username_message
-    );
-  });
-  test("Empty User Empty Password", async ({ page }) => {
-    await page.goto(websiteURL.baseURL);
-    await page.locator(testLocators.login_button).click();
-    await expect(page.locator(testLocators.error_page_locator)).toHaveText(
-      testAssertions.requared_username_message
-    );
   });
 });
