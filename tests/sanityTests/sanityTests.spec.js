66841519f7d6ac6caf74c0c4d11255b3747065e6
Aleksandr Arkhipov <130854915+sinrabanse@users.noreply.github.com>
2024-11-21 16:16:23 +0200

    Finished

--- tests/sanityTests/sanityTests.spec.js
+++ tests/sanityTests/sanityTests.spec.js
@@ -3,7 +3,7 @@ import { testData } from "../utils/testData.js";
 import { testLocators } from "../utils/testLocators.js";
 import { websiteURL } from "../utils/testURL.js";
 
-test.describe.only("sanityTest1", () => {
+test.describe("sanityTest1", () => {
   test("Valid", async ({ page }) => {
     await page.goto(websiteURL.baseURL);
     await page
@@ -12,45 +12,52 @@ test.describe.only("sanityTest1", () => {
     await page.locator(testLocators.password_field).fill(testData.password);
     await page.locator(testLocators.login_button).click();
     await expect(page.locator(testLocators.title_page_locator)).toHaveText(
-      "Products"
+      testData.title_products_page
     );
-    await page.locator(testLocators.addToCartButton_1).click();
-    await page.locator(testLocators.addToCartButton_2).click();
+    await page
+      .locator(testLocators.productCard_1)
+      .locator(testLocators.addToCartButton_1)
+      .click();
+    await page
+      .locator(testLocators.productCard_2)
+      .locator(testLocators.addToCartButton_2)
+      .click();
     await expect(page.locator(testLocators.cartNumber)).toHaveText("2");
     await page.locator(testLocators.cartButton).click();
     await expect(page).toHaveURL(websiteURL.cartURL);
     await expect(page.locator(testLocators.title_page_locator)).toHaveText(
-      "Your Cart"
+      testData.title_cart_page
     );
     await expect(page.locator(testLocators.cart_items)).toHaveCount(2);
     await page.locator(testLocators.checkoutButton).click();
     await expect(page).toHaveURL(websiteURL.checkoutURL);
     await expect(page.locator(testLocators.title_page_locator)).toHaveText(
-      "Checkout: Your Information"
+      testData.title_checkout_page
     );
-    await page.locator(testLocators.firstNameLocator).fill("Aleks");
-    await page.locator(testLocators.lastNameLocator).fill("Arkhipov");
-    await page.locator(testLocators.postalCodeLocator).fill("4455666");
+    await page
+      .locator(testLocators.firstNameLocator)
+      .fill(testData.customer_first_name);
+    await page
+      .locator(testLocators.lastNameLocator)
+      .fill(testData.customer_last_name);
+    await page
+      .locator(testLocators.postalCodeLocator)
+      .fill(testData.customer_postal_code);
     await page.locator(testLocators.checkoutContinueButton).click();
     await expect(page).toHaveURL(websiteURL.checkoutsteptwoURL);
     await expect(page.locator(testLocators.title_page_locator)).toHaveText(
-      "Checkout: Overview"
+      testData.title_checkout_overview
     );
     await page.locator(testLocators.finishButton).click();
     await expect(page).toHaveURL(websiteURL.finishURL);
     await expect(page.locator(testLocators.title_page_locator)).toHaveText(
-      "Checkout: Complete!"
+      testData.title_checkout_complete
     );
     await expect(page.locator(testLocators.thankYouMessageLocator)).toHaveText(
-      "Thank you for your order!"
+      testData.thank_you_message
     );
     await expect(
       page.locator(testLocators.underThankYouMessageLocator)
-    ).toHaveText(
-      "Your order has been dispatched, and will arrive just as fast as the pony can get there!"
-    );
-    console.log("Finish of test");
-
-    //await page.pause();
+    ).toHaveText(testData.under_thank_you_message);
   });
 });
--- tests/utils/testData.js
+++ tests/utils/testData.js
@@ -10,4 +10,15 @@ export const testData = {
   productName_2: "Sauce Labs Fleece Jacket",
   incorrect_password: "secret_sauc",
   incorrect_standard_user: "standard_use",
+  title_products_page: "Products",
+  title_cart_page: "Your Cart",
+  title_checkout_page: "Checkout: Your Information",
+  title_checkout_overview: "Checkout: Overview",
+  title_checkout_complete: "Checkout: Complete!",
+  thank_you_message: "Thank you for your order!",
+  under_thank_you_message:
+    "Your order has been dispatched, and will arrive just as fast as the pony can get there!",
+  customer_first_name: "Aleks",
+  customer_last_name: "Arkhipov",
+  customer_postal_code: "4455666",
 };
