6b5f4529d052201aa795d0d639b76e23308ba000
Aleksandr Arkhipov <130854915+sinrabanse@users.noreply.github.com>
2024-11-25 00:56:07 +0200

    fixed after code review

--- tests/loginTests/positiveTests/ValidateUsersLogin.spec.js
+++ tests/loginTests/positiveTests/ValidateUsersLogin.spec.js
@@ -1,28 +1,19 @@
-import {expect, test} from '@playwright/test'
-import {testData} from '../../utils/testData.js'
-import {testLocators} from '../../utils/testLocators.js'
-import {websiteURL} from '../../utils/testURL.js'
+import { expect, test } from "@playwright/test";
+import { testData, validUsers } from "../../utils/testData.js";
+import { testLocators } from "../../utils/testLocators.js";
+import { websiteURL } from "../../utils/testURL.js";
+import { testUIElements } from "../../utils/testUIElements.js";
 
-const users = [
-    testData.standard_user,
-    testData.error_user,
-    testData.performance_glitch_user,
-    testData.problem_user,
-    testData.visual_user,
-] // Why not separating the users and just import them here and then iterate on them directly.
-
-test.describe('loginUsers', () => {
-    for (const user of users) {
-        test(`Testing with ${user}`, async ({page}) => {
-            await page.goto(websiteURL.baseURL)
-            await page.locator(testLocators.username_field).fill(`${user}`)
-            await page
-                .locator(testLocators.password_field)
-                .fill(testData.password)
-            await page.locator(testLocators.login_button).click()
-            await expect(
-                page.locator(testLocators.title_page_locator),
-            ).toHaveText('Products')
-        })
-    }
-})
+test.describe("Validation valid users", () => {
+  for (const user of validUsers) {
+    test(`Testing with ${user}`, async ({ page }) => {
+      await page.goto(websiteURL.baseURL);
+      await page.locator(testLocators.username_field).fill(`${user}`);
+      await page.locator(testLocators.password_field).fill(testData.password);
+      await page.locator(testLocators.login_button).click();
+      await expect(page.locator(testLocators.title_page_locator)).toHaveText(
+        testUIElements.title_products_page
+      );
+    });
+  }
+});
--- tests/utils/testData.js
+++ tests/utils/testData.js
@@ -1,26 +1,24 @@
 export const testData = {
-    standard_user: 'standard_user',
-    error_user: 'error_user',
-    performance_glitch_user: 'performance_glitch_user',
-    visual_user: 'visual_user',
-    problem_user: 'problem_user',
-    locked_out_user: 'locked_out_user',
-    password: 'secret_sauce',
-    // Start: Seperate web text/elements from testData files
-    productName_1: 'Sauce Labs Backpack',
-    productName_2: 'Sauce Labs Fleece Jacket',
-    incorrect_password: 'secret_sauc',
-    incorrect_standard_user: 'standard_use',
-    title_products_page: 'Products',
-    title_cart_page: 'Your Cart',
-    title_checkout_page: 'Checkout: Your Information',
-    title_checkout_overview: 'Checkout: Overview',
-    title_checkout_complete: 'Checkout: Complete!',
-    thank_you_message: 'Thank you for your order!',
-    under_thank_you_message:
-        'Your order has been dispatched, and will arrive just as fast as the pony can get there!',
-    // End: Seperate web text/elements from testData files
-    customer_first_name: 'Aleks',
-    customer_last_name: 'Arkhipov',
-    customer_postal_code: '4455666',
-}
+  standard_user: "standard_user",
+  error_user: "error_user",
+  performance_glitch_user: "performance_glitch_user",
+  visual_user: "visual_user",
+  problem_user: "problem_user",
+  locked_out_user: "locked_out_user",
+  password: "secret_sauce",
+  incorrect_password: "secret_sauc",
+  incorrect_standard_user: "standard_use",
+  customer_first_name: "Aleks",
+  customer_last_name: "Arkhipov",
+  customer_postal_code: "4455666",
+};
+
+export const validUsers = [
+  testData.standard_user,
+  testData.error_user,
+  testData.performance_glitch_user,
+  testData.problem_user,
+  testData.visual_user,
+];
+
+export const invalidUsers = [testData.locked_out_user];
